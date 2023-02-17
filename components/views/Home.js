9618fc98856f73a5d18b70f8ba8eeac5f8eb182f
Joshua Ochsankehl <eternity336@gmail.com>
2023-02-16 20:27:00 -0600

    Class Activity SPA Work during Week 6

---
+++ components/views/Home.js
@@ -0,0 +1,9 @@
+import html from "html-literal";
+// import sharkImage from "../../assets/img/photo-1528334713982-2d1fe11a157a.jpg";
+
+export default () => html`
+<section id="jumbotron">
+    <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
+    <a href="index.html">"Call to Action" "Button"</a>
+</section>
+`;
