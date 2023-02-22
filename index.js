f5ef5245a479b8feeea0d4ac3a76aaa8f5829a4b
Joshua Ochsankehl <eternity336@gmail.com>
2023-02-21 20:51:48 -0600

    Updated class activities

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-import { Header, Footer, Nav, Main } from './components';
+import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
+import dotenv from "dotenv";
 
 const router = new Navigo("/");
+dotenv.config();
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+  ${Header(state)}
+  ${Nav(store.Links)}
+  ${Main(state)}
+  ${Footer()}
+  `;
+  afterRender();
+  router.updatePageLinks();
+}
 
 function afterRender() {
   // add menu toggle to bars icon in nav bar
@@ -12,24 +26,61 @@ function afterRender() {
   });
 }
 
-function render(state = store.Home) {
-  document.querySelector("#root").innerHTML = `
-    ${Header(state)}
-    ${Nav(store.Links)}
-    ${Main(state)}
-    ${Footer()}
-  `;
-  afterRender();
-  router.updatePageLinks();
-}
+router.hooks({
+  before: (done, params) => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home"; // Add a switch case statement to handle multiple routes
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Home":
+        axios
+          .get(
+            // Replace the key provided here with your own key from openweathermap
+            `https://api.openweathermap.org/data/2.5/weather?q=st%20louis&appid=${process.env.API_KEY}`
+          )
+          .then(response => {
+            console.log(response.data);
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
 
-router.on("/", () => render(store.Home)).resolve();
+            // Save Data into state
+            store.Home.weather = {};
+            store.Home.weather.city = response.data.name;
+            store.Home.weather.temp = kelvinToFahrenheit(
+              response.data.main.temp
+            );
+            store.Home.weather.feelsLike = kelvinToFahrenheit(
+              response.data.main.feels_like
+            );
+            store.Home.weather.description =
+              response.data.weather[0].description;
+            console.log(store.Home.weather);
+
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    render(store[view]);
+  }
+});
+
 router
   .on({
     "/": () => render(),
-    ":view": (params) => {
+    ":view": params => {
       let view = capitalize(params.data.view);
       render(store[view]);
-    },
+    }
   })
   .resolve();
